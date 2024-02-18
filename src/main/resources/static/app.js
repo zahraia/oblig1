cd755e6914dad22a0df4e1aa163d276dfd765dd6
zahraia <143134693+zahraia@users.noreply.github.com>
2024-02-18 22:00:00 +0100

    lat

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,6 +1,9 @@
 
+   // creates an empty array to put all orders in and fetches the table to put them in
     let orders = [];
     const tickets = document.getElementById("tickets");
+
+    //validates email using regex
     function validateEmail(email){
         const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return pattern.test(email);
@@ -11,6 +14,7 @@
         return pattern.test(phone);
     }
 
+    //buys ticket by getting values and checking if email and phone is validated
     function buyTicket(){
 
     const alert_phone = document.getElementById("err_phone");
@@ -21,7 +25,6 @@
     const alert_movie = document.getElementById("err_movie");
 
 
-
     const first_name = document.getElementById("first_name");
     const surname = document.getElementById("surname");
     const movie = document.getElementById("movies");
@@ -29,12 +32,12 @@
     const phone = document.getElementById("phone");
     const email = document.getElementById("email");
 
-        alert_email.innerHTML = "";
-        alert_phone.innerHTML = "";
-        alert_surname.innerHTML = "";
-        alert_first_name.innerHTML = "";
-        alert_amount.innerHTML = "";
-        alert_movie.innerHTML = "";
+        alert_email.textContent = "";
+        alert_phone.textContent = "";
+        alert_surname.textContent = "";
+        alert_first_name.textContent = "";
+        alert_amount.textContent = "";
+        alert_movie.textContent = "";
 
 
     if (validateEmail(email.value) && validatePhone(phone.value)){
@@ -54,43 +57,42 @@
 
 
     }
+    //validates all input
     else {
     if (email.value === ""){
-        alert_email.innerHTML = "Please enter your email.";
+        alert_email.textContent = "Please enter your email.";
     }
 
     else if(!validateEmail(email.value)){
-        alert_email.innerHTML = "Please enter a valid email.";
+        alert_email.textContent = "Please enter a valid email.";
     }
 
     if (phone.value === ""){
-        alert_phone.innerHTML = "Please enter your phone number.";
+        alert_phone.textContent = "Please enter your phone number.";
     }
 
     else if(!validatePhone(phone.value)){
-        alert_phone.innerHTML = "Please enter a valid phone number."
+        alert_phone.textContent= "Please enter a valid phone number."
     }
 
     if (first_name.value === ""){
-        alert_first_name.innerHTML = "Please enter your first name.";
+        alert_first_name.textContent = "Please enter your first name.";
     }
 
     if (surname.value === ""){
-        alert_surname.innerHTML = "Please enter your surname.";
+        alert_surname.textContent = "Please enter your surname.";
     }
     if (parseInt(amount.value) <= 0 || isNaN(parseInt(amount.value))){
-        alert_amount.innerHTML = "Please enter a valid amount.";
-        console.log("Amount:", amount.value);
-        console.log("Parsed Amount:", parseInt(amount.value));
+        alert_amount.textContent = "Please enter a valid amount.";
     }
     if (movie.value === ""){
-        alert_movie.innerHTML = "Please choose your movie.";
+        alert_movie.textContent = "Please choose your movie.";
     }
 
         }
 
 }
-
+//creates a table haed and shows the array
     function showList(){
 
     tickets.innerHTML = "";
@@ -104,15 +106,15 @@
     "<th>" + "Email" + "</th>" +
     "</tr>";
 
-    for (let i = 0; i < orders.length; i++){
-    ut += "<tr>" + "<td>" + orders[i].movie + "</td>" + "<td>" + orders[i].amount +  "</td>" + "<td>" + orders[i].first_name +
-    "</td>" + "<td>" + orders[i].surname + "</td>" + "<td>" + orders[i].phone + "</td>" + "<td>" + orders[i].email +" </td>"+ "</tr>";
+    for (let i of orders) {
+    ut += "<tr>" + "<td>" + i.movie + "</td>" + "<td>" + i.amount +  "</td>" + "<td>" + i.first_name +
+    "</td>" + "<td>" + i.surname + "</td>" + "<td>" + i.phone + "</td>" + "<td>" + i.email +" </td>"+ "</tr>";
 
 }
     tickets.innerHTML += ut;
 }
 
-
+//empties the array and deletes the content of the table
     function deleteTickets() {
     orders = [];
     tickets.innerHTML = "";
